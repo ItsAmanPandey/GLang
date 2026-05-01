897062d1a3408ce7313ebcef19488a1f32e751e8
Aman Aneet Pandey <aman-aneet.pandey@informatik.hs-fulda.de>
2026-05-01 02:28:51 +0200

    feat(Analytics): Added the Google Analystics Script

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,20 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 
+// --- Google Analytics (loaded from env variable) ---
+const GA_ID = import.meta.env.VITE_GA_ID;
+if (GA_ID) {
+  const script = document.createElement("script");
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { window.dataLayer.push(arguments); }
+  gtag("js", new Date());
+  gtag("config", GA_ID);
+}
+
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
